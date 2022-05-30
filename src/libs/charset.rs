904b4d0388d293299966fc5be925df64b181e4a2
vlwkaos <mgparkprint@gmail.com>
2022-05-30 15:46:09 +0900

    refactor: just clone range

--- src/libs/charset.rs
+++ src/libs/charset.rs
@@ -1,15 +1,6 @@
 use std::ops::{Range, RangeInclusive};
 use rand::Rng;
 
-const ALPHA_L_RANGE: [RangeInclusive<char>;1] = ['\u{0061}'..='\u{007A}'];
-const ALPHA_U_RANGE: [RangeInclusive<char>;1] = ['\u{0041}'..='\u{005A}'];
-const NUMSYM_RANGE: [RangeInclusive<char>;1] = ['\u{0021}'..='\u{0040}'];
-const KATAKANA_RANGE: [RangeInclusive<char>;1] = ['\u{30A0}'..='\u{30FF}'];   
-const EMOJI_RANGE: [RangeInclusive<char>;5] = ['\u{1F000}'..='\u{1F02F}', 
-                                           '\u{1F0A0}'..='\u{1F0FF}', 
-                                           '\u{1F680}'..='\u{1F6FF}',
-                                           '\u{1F910}'..='\u{1F96B}', 
-                                           '\u{1F980}'..='\u{1F9E0}'];
 #[derive(Debug, Clone)]
 pub enum Charset {
   AlphaLowercase,
@@ -20,14 +11,18 @@ pub enum Charset {
 }
 
 impl Charset {
-  pub fn get_charset_range(&self) -> Vec<RangeInclusive<char>> {
+  pub fn get_charset_range(&self) -> Box<[RangeInclusive<char>]> {
     // non-primitive type return
     match *self {
-      Charset::AlphaLowercase => ALPHA_L_RANGE.into(),
-      Charset::AlphaUppercase => ALPHA_U_RANGE.into(),
-      Charset::NumSym => NUMSYM_RANGE.into(),
-      Charset::Katakana => KATAKANA_RANGE.into(), 
-      Charset::Emoji => EMOJI_RANGE.into(),
+      Charset::AlphaLowercase => ['\u{0061}'..='\u{007A}'].into(),
+      Charset::AlphaUppercase => ['\u{0041}'..='\u{005A}'].into(),
+      Charset::NumSym => ['\u{0021}'..='\u{0040}'].into(),
+      Charset::Katakana => ['\u{30A0}'..='\u{30FF}'].into(), 
+      Charset::Emoji => ['\u{1F000}'..='\u{1F02F}', 
+                         '\u{1F0A0}'..='\u{1F0FF}', 
+                         '\u{1F680}'..='\u{1F6FF}',
+                         '\u{1F910}'..='\u{1F96B}', 
+                         '\u{1F980}'..='\u{1F9E0}'].into(),
     }
   }
 
@@ -35,10 +30,8 @@ impl Charset {
     let mut rng = rand::thread_rng();
     let charset = self.get_charset_range();
     let idx = rng.gen_range(0..charset.len());
-    // gen_range는 reference로 할 수 없다..
-    match charset.into_iter().nth(idx) {
-      Some(char_range) => rng.gen_range(char_range),
-      _ => ' '
-    }
+    // Range does not implement iterator over immutable reference..
+    // cloning is cheap for range. just use it
+    rng.gen_range(charset[idx].clone())
   }
 }
